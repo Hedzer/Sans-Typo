f0559646b430f38dd83332388bad6456e849342e
Hedzer Ferwerda <hedzer87@gmail.com>
2017-11-29 13:15:44 -0500

    Removed external references from tracking

--- External/1.0.0/Components/TypeSpeed/Tester/Stats.js
+++
@@ -1,6 +0,0 @@
-
-import imports from '/Parcello/imports';
-
-let imported = imports('/SansTypo/Source/1.0.0/Components/TypeSpeed/Tester/Stats');
-
-export default imported;
--- External/1.0.0/Features/TypeSpeed.js
+++
@@ -1,6 +0,0 @@
-
-import imports from '/Parcello/imports';
-
-let imported = imports('/SansTypo/Source/1.0.0/Features/TypeSpeed');
-
-export default imported;
