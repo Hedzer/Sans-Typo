b7d7310517039ff60b3f0e748605dc50b095d223
Hedzer Ferwerda <hedzer87@gmail.com>
2017-11-30 11:09:35 -0500

    Added core functionality

--- Source/1.0.0/Components/TypeSpeed/Tester/Word.js
+++ Source/1.0.0/Components/TypeSpeed/Tester/Word.js
@@ -6,32 +6,38 @@ import Span from 'JSUI/Source/1.0.0/Classes/Elements/Span';
 //Constants
 import settings from 'SansTypo/Source/1.0.0/Constants/settings';
 
+//Components
+import Letter from 'SansTypo/Source/1.0.0/Components/TypeSpeed/Tester/Word';
+
 //Utilities
 import exports from 'Parcello/exports';
 
 const identity = new Identity({
 	namespace: settings.namespace,
-	class: 'Words',
+	class: 'Word',
 	major: 1, minor: 0, patch: 0,
 });
 
-class Words extends Span {
+class Word extends Span {
 	constructor() {
 		super();
 		this.identity = identity;
 	}
-	
-	construct_structure() {
-
-	}
-	construct_style() {
-
-	}
-	construct_relationships() {
-
+	revise(correctWord) {
+		let text = this.text()
+		let length = (correctWord.length > text.length ? text.length : correctWord.length);
+		let letters = [];
+		for (let i = 0; i < length; i++) {
+			let letter = new Letter();
+			letter.text(text.charAt(i));
+			letter.isIncorrect = (text.charAt(i) !== correctWord.charAt(i));
+			letters.push(letter);
+		}
+		this.element.innerHTML = '';
+		this.add(letters);
 	}
 }
 
-export default Words;
+export default Word;
 
-exports(Words).as('SansTypo/Source/1.0.0/Components/TypeSpeed/Tester/Words');
+exports(Word).as('SansTypo/Source/1.0.0/Components/TypeSpeed/Tester/Word');
