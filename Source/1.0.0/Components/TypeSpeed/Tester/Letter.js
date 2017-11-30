81c40c459d50f567110fc1a24c282b4bcb7c8e4e
Hedzer Ferwerda <hedzer87@gmail.com>
2017-11-30 11:11:01 -0500

    Added Letter, partial core functionality

---
+++ Source/1.0.0/Components/TypeSpeed/Tester/Letter.js
@@ -0,0 +1,38 @@
+
+//Classes
+import Identity from 'JSUI/Source/1.0.0/Classes/Core/Identity';
+import Span from 'JSUI/Source/1.0.0/Classes/Elements/Span';
+
+//Constants
+import settings from 'SansTypo/Source/1.0.0/Constants/settings';
+
+//Utilities
+import exports from 'Parcello/exports';
+
+const identity = new Identity({
+	namespace: settings.namespace,
+	class: 'Letter',
+	major: 1, minor: 0, patch: 0,
+});
+
+class Letter extends Span {
+	constructor() {
+		super();
+		this.identity = identity;
+	}
+
+	get isIncorrect() {
+		return this.state('isIncorrect');
+	}
+	set isIncorrect(value) {
+		value = !!value;
+		let hasChanged = this.state('isIncorrect', value); // added for clarity
+		if (hasChanged) {
+			this.class('is-incorrect')[value ? 'add' : 'remove']();
+		}
+	}
+}
+
+export default Letter;
+
+exports(Letter).as('SansTypo/Source/1.0.0/Components/TypeSpeed/Tester/Letter');
