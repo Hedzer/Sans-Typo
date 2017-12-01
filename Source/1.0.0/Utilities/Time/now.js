a5087db95638f2656646929f35ecb7a74553db8f
Hedzer Ferwerda <hedzer87@gmail.com>
2017-12-01 01:14:46 -0500

    Added now utility

---
+++ Source/1.0.0/Utilities/Time/now.js
@@ -0,0 +1,11 @@
+
+//Utilities
+import exports from 'Parcello/exports';
+
+function now() {
+	return new Date().getTime();
+}
+
+export default now;
+
+exports(now).as('SansTypo/Source/1.0.0/Utilities/now');
