0918199b0387ac0561ef3be520c18233aa29efbf
Hedzer Ferwerda <hedzer87@gmail.com>
2017-12-01 21:51:54 -0500

    Added quotes data & random quote delivery

--- Data/quotes.js
+++ Data/quotes.js
@@ -1,4 +1,49 @@
 
-const quotes = [
+//Utilities
+import exports from 'Parcello/exports';
 
+const quotes = [
+	"Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.",
+	"I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed.",
+	"Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do, so throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails.  Explore, Dream, Discover.",
+	"The most common way people give up their power is by thinking they don’t have any.",
+	"The best time to plant a tree was 20 years ago. The second best time is now.",
+	"Every child is an artist.  The problem is how to remain an artist once he grows up.",
+	"You can never cross the ocean until you have the courage to lose sight of the shore.",
+	"I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
+	"The two most important days in your life are the day you are born and the day you find out why.",
+	"Whatever you can do, or dream you can, begin it.  Boldness has genius, power and magic in it.",
+	"People often say that motivation doesn’t last. Well, neither does bathing.  That’s why we recommend it daily.",
+	"If you hear a voice within you say “you cannot paint,” then by all means paint and that voice will be silenced.",
+	"Ask and it will be given to you; search, and you will find; knock and the door will be opened for you.",
+	"When I stand before God at the end of my life, I would hope that I would not have a single bit of talent left and could say, I used everything you gave me.",
+	"Few things can help an individual more than to place responsibility on him, and to let him know that you trust him.",
+	"We can easily forgive a child who is afraid of the dark; the real tragedy of life is when men are afraid of the light.",
+	"When I was 5 years old, my mother always told me that happiness was the key to life.  When I went to school, they asked me what I wanted to be when I grew up.  I wrote down ‘happy’.  They told me I didn’t understand the assignment, and I told them they didn’t understand life.",
+	"When one door of happiness closes, another opens, but often we look so long at the closed door that we do not see the one that has been opened for us.",
+	"How wonderful it is that nobody need wait a single moment before starting to improve the world.",
+	"Life is not measured by the number of breaths we take, but by the moments that take our breath away.",
+	"If you’re offered a seat on a rocket ship, don’t ask what seat! Just get on.",
+	"First, have a definite, clear practical ideal; a goal, an objective. Second, have the necessary means to achieve your ends; wisdom, money, materials, and methods. Third, adjust all your means to that end.",
+	"You can’t fall if you don’t climb.  But there’s no joy in living your whole life on the ground.",
+	"We must believe that we are gifted for something, and that this thing, at whatever cost, must be attained.",
+	"I have been impressed with the urgency of doing. Knowing is not enough; we must apply. Being willing is not enough; we must do.",
+	"Limitations live only in our minds.  But if we use our imaginations, our possibilities become limitless.",
+	"You take your life in your own hands, and what happens? A terrible thing, no one to blame.",
+	"What’s money? A man is a success if he gets up in the morning and goes to bed at night and in between does what he wants to do.",
+	"In order to succeed, your desire for success should be greater than your fear of failure.",
+	"The person who says it cannot be done should not interrupt the person who is doing it.",
+	"A truly rich man is one whose children run into his arms when his hands are empty.",
+	"It is not what you do for your children, but what you have taught them to do for themselves, that will make them successful human beings.",
+	"If you want your children to turn out well, spend twice as much time with them, and half as much money.",
+	"The battles that count aren’t the ones for gold medals. The struggles within yourself–the invisible battles inside all of us–that’s where it’s at.",
+	"I have learned over the years that when one’s mind is made up, this diminishes fear.",
+	"If you look at what you have in life, you’ll always have more. If you look at what you don’t have in life, you’ll never have enough.",
+	"It’s your place in the world; it’s your life. Go on and do all you can with it, and make it the life you want to live.",
+	"The question isn’t who is going to let me; it’s who is going to stop me.",
+	"When everything seems to be going against you, remember that the airplane takes off against the wind, not with it.",
 ];
+
+export default quotes;
+
+exports(quotes).as('SansTypo/Data/quotes');
--- Source/1.0.0/Server/getNewPhrase.js
+++ Source/1.0.0/Server/getNewPhrase.js
@@ -1,8 +1,13 @@
 
+//Data
+import quotes from 'SansTypo/Data/quotes';
+
 export default function getNewPhrase() { // set up to mock random latency
 	return new Promise( resolve => {
 		setTimeout(() => {
-			resolve("this is a test quote");
+			let random = Math.ceil(Math.random() * quotes.length + 1) % quotes.length;
+			let quote = quotes[random];
+			resolve(quote);
 		}, Math.random() * 200);
 	})
 }
